5fe8bdc0f021a17ba7c7cbea1e4d530025f94bc0
Abongile21 <abongiletheo00@gmail.com>
2024-05-04 15:27:21 +0200

    created a Moviefinder client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './comps/home/home.component';
+import { MovieComponent } from './comps/movie/movie.component';
+import { MoviesComponent } from './comps/movies/movies.component';
+import { WishlistComponent } from './comps/wishlist/wishlist.component';
+import { RecommendationsComponent } from './comps/recommendations/recommendations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    MoviesComponent,
+    WishlistComponent,
+    RecommendationsComponent
   ],
   imports: [
     BrowserModule,
