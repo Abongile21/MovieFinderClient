a72c2fe4310d137568f8428809b3aa14d53e9056
DiniJNS <jonas.dinilethu@gmail.com>
2024-05-07 11:09:09 +0200

    added to wishlist

--- src/app/comps/wishlist/wishlist.component.ts
+++ src/app/comps/wishlist/wishlist.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrl: './wishlist.component.css'
+  styleUrls: './wishlist.component.css'
 })
 export class WishlistComponent {
+  movies: Movie[] = []; // Initialize an empty array to store movies
 
+  // Define a Movie interface to represent the structure of the movie
+  // interface Movie{
+  //   title : string;
+  //   Year : number;
+  //   genre : string;
+  //   poster : string;
+
+  // }
+ addToWishlist(movieId: number): void{
+  const movie = this.movies.find(m => m.id === movieId);
+  if (movie && !this.addToWishlist.some(m => m.id === movie.id)){
+    this.wishlist.push(movie);
+  }
+ }
+  // Method to remove movies from the wishlist
+  removeFromWishlist(movie : Movie): void{
+    const index = this.movies.findIndex(m => m.title === movie.title );
+    if (index !== -1) {
+      this.movies.splice(index, 1);
+    }
+  }
 }
