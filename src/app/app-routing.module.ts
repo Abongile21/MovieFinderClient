c4c1c0ba0b311ccf38b519a5dec47590ff5eea4a
unknown <abongiletheo00@gmail.com>
2024-05-09 01:52:33 +0200

    styled the wishlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './comps/home/home.component';
 import { WishlistComponent } from './comps/wishlist/wishlist.component';
 import { RecommendationsComponent } from './comps/recommendations/recommendations.component';
+import { MovieComponent } from './comps/movie/movie.component';
 
 const routes: Routes = [
   {path : "", redirectTo: "home", pathMatch: "full"},
   {path: "home", component: HomeComponent},
   {path: "wishlist", component: WishlistComponent},
   {path: "recommendations", component: RecommendationsComponent},
+  {path: "movie", component: MovieComponent }
+
   
   
 ];
