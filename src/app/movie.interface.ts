36bbefbd7afe2cb79a626c24e6769d0a7d713131
Abongile21 <abongiletheo00@gmail.com>
2024-05-08 14:21:58 +0200

    edited movies'

--- src/app/movie.interface.ts
+++ src/app/movie.interface.ts
@@ -1,9 +1,12 @@
 
   export interface Movie{
     id: number
-    title : string;
-    Year : number;
-    genre : string;
-    poster : string;
-
+    title: string
+   image:string
+   genre: string
+   year: string
+  language:string
+  description:string
+  YTUrl:string
+      
 }
