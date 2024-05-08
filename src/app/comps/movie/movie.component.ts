28ea24f407e565d4bfa90d5819f27ddab6977d93
unknown <abongiletheo00@gmail.com>
2024-05-09 00:22:54 +0200

    Andd changes on home

--- src/app/comps/movie/movie.component.ts
+++ src/app/comps/movie/movie.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../../services/movie.service'; 
+import { Movie } from '../../movie.interface';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrl: './movie.component.css'
+  styleUrls: ['./movie.component.css'] 
 })
 export class MovieComponent {
+
+  movies: any = [];
+  filteredMovies: Movie[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  getAllMovies(): void {
+    this.movieService.getMovies().subscribe({
+      next: (data: any) => {
+        this.movies = data.movies;
+        this.filteredMovies = this.movies;
+      },
+      error: (err) => {
+        console.error("Could not retrieve movies", err);
+      }
+    });
+  }
+
+  getOneMovie(movieId: string): void {
+    this.movieService.getMovieById(movieId).subscribe({
+      next: (data: any) => {
+        console.log("Movie details:", data);
+      },
+      error: (err) => {
+        console.error("Could not retrieve movie details", err);
+      }
+    });
+  }
 
 }
